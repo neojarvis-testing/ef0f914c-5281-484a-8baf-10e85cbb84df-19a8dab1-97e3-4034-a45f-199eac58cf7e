daa5adbcd25acd0a5a3a92c79752b7932be0b92b
pathak2103 <pathakshashank021@gmail.com>
2025-04-29 07:21:02 +0000

    ignore

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UseraddfeedbackComponent } from './components/useraddfeedback/useraddfeedback.component';
+import { UsernavComponent } from './components/usernav/usernav.component';
+import { FormsModule } from '@angular/forms';
+import { UserappliedinternshipComponent } from './components/userappliedinternship/userappliedinternship.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UseraddfeedbackComponent,
+    UsernavComponent,
+    UserappliedinternshipComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angularapp/src/app/components/useraddfeedback/useraddfeedback.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-useraddfeedback',
+  templateUrl: './useraddfeedback.component.html',
+  styleUrls: ['./useraddfeedback.component.css']
+})
+export class UseraddfeedbackComponent implements OnInit {
+ 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+}
+
+
+
