8447f8aad9c0e34d9cf1d07f94a170514ade15e7
yaswanthreddy45 <yyaswanth515@gmail.com>
2025-04-29 07:19:59 +0000

    ignore

---
+++ angularapp/src/app/models/internship.model.ts
@@ -0,0 +1,12 @@
+export interface Internship {
+    InternshipId?: number;
+    Title: string;
+    CompanyName: string;
+    Location: string;
+    DurationInMonths: number;
+    Stipend: number;
+    Description: string;
+    SkillsRequired: string;
+    ApplicationDeadline: string;
+   }
+   
